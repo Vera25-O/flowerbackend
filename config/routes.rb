20beccb2329ca6b0b7fb7c306174bb7ac86f0fee
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-10-13 14:31:42 +0300

    modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  post "/login", to: "sessions#create"
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
