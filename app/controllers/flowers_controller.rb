e4556f5e5ad2b77249784f1ec88557b78404a1be
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-10-14 01:24:28 +0300

    modified flowers_controller.rb

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -1,2 +1,14 @@
 class FlowersController < ApplicationController
+    # def create 
+    #     hero=HeroPower.create!(hero_power_params)
+    #     render json: hero.power
+    # end
+    def index
+        render json: Flower.all, status: :ok
+    end
+
+    def show
+        flower=Flower.find_by!(id:params[:id])
+        render json: flower, status: :ok
+    end
 end
